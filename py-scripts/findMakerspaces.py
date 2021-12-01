e4895f1eff76ec58d4514a3158876563c42e9a7e
makerinchief <info@makerinchief.com>
2021-12-01 13:56:23 -0500

    JSON Update
    Refactored Python scripts to return a better formatted JSON.  JSON now has a Title and Link for each makerspace in each state.

--- py-scripts/findMakerspaces.py
+++ py-scripts/findMakerspaces.py
@@ -1,77 +1,103 @@
-from googlesearch import search
+from requests import get
+from bs4 import BeautifulSoup
 import json
 from time import sleep
+import os
 
-makerspaceList = {
-    "Alabama": [],
-    "Alaska": [],
-    "Arizona": [],
-    "Arkansas": [],
-    "California": [],
-    "Colorado": [],
-    "Connecticut": [],
-    "Delaware": [],
-    "Florida": [],
-    "Georgia": [],
-    "Hawaii": [],
-    "Idaho": [],
-    "Illinois": [],
-    "Indiana": [],
-    "Iowa": [],
-    "Kansas": [],
-    "Kentucky": [],
-    "Louisiana": [],
-    "Maine": [],
-    "Maryland": [],
-    "Massachusetts": [],
-    "Michigan": [],
-    "Minnesota": [],
-    "Mississippi": [],
-    "Missouri": [],
-    "Montana": [],
-    "Nebraska": [],
-    "Nevada": [],
-    "New Hampshire": [],
-    "New Jersey": [],
-    "New Mexico": [],
-    "New York": [],
-    "North Carolina": [],
-    "North Dakota": [],
-    "Ohio": [],
-    "Oklahoma": [],
-    "Oregon": [],
-    "Pennsylvania": [],
-    "Rhode Island": [],
-    "South Carolina": [],
-    "South Dakota": [],
-    "Tennessee": [],
-    "Texas": [],
-    "Utah": [],
-    "Vermont": [],
-    "Virginia": [],
-    "Washington": [],
-    "West Virginia": [],
-    "Wisconsin": [],
-    "Wyoming": [],
-}
+state_list = [
+    "alabama",
+    "alaska",
+    "arizona",
+    "arkansas",
+    "california",
+    "colorado",
+    "connecticut",
+    "delaware",
+    "florida",
+    "georgia",
+    "hawaii",
+    "idaho",
+    "illinois",
+    "indiana",
+    "iowa",
+    "kansas",
+    "kentucky",
+    "louisiana",
+    "maine",
+    "maryland",
+    "massachusetts",
+    "michigan",
+    "minnesota",
+    "mississippi",
+    "missouri",
+    "montana",
+    "nebraska",
+    "nevada",
+    "new Hampshire",
+    "new Jersey",
+    "new Mexico",
+    "new York",
+    "north Carolina",
+    "north Dakota",
+    "ohio",
+    "oklahoma",
+    "oregon",
+    "pennsylvania",
+    "rhode Island",
+    "south Carolina",
+    "south Dakota",
+    "tennessee",
+    "texas",
+    "utah",
+    "vermont",
+    "virginia",
+    "washington",
+    "west Virginia",
+    "wisconsin",
+    "wyoming",
+]
+
+maker_list = {}
 
 
-print(makerspaceList.keys(), makerspaceList.values())
+def filter_results(raw_html):
+    soup = BeautifulSoup(raw_html, "html.parser")
+    result_block = soup.find_all("div", attrs={"class": "g"})
+    search_results = []
+    print(len(result_block))
+    for result in result_block:
+        link = result.find("a", href=True)
+        title = result.find("h3")
+        if link and title:
+            print(f"FOUND:\nTITLE: {title.text} \nLINK: {link['href']}")
+            arr = [title.text, link["href"]]
+            search_results.append(arr)
 
-googleSearch = ""
+    return search_results
 
-for key in makerspaceList.keys():
-    currentState = key
-    googleSearch = currentState + " makerspace"
-    print(f"Searching for {googleSearch} space in {currentState}")
 
-    for j in search(googleSearch, tld="com", num=20, stop=20, pause=4):
-        print(currentState, j)
-        makerspaceList[key].append(j)
+def search_google(state, num_results):
+    user_agent = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/61.0.3163.100 Safari/537.36"
+    }
 
-    print(f"Saving JSON")
-    with open("stateList.json", "r+") as json_file:
-        json.dump(makerspaceList, json_file)
+    search_term = state + "+makerspace"
+    google_url = "https://www.google.com/search?q={}&num={}&hl={}".format(
+        search_term, num_results, "en"
+    )
+    print(google_url)
+    response = get(google_url, headers=user_agent, proxies=None)
+    response.raise_for_status()
+    return response.text
 
-    print(key, " FOUND")
-    sleep(2)
+
+for state in state_list:
+    print(f"Finding spaces in {state}")
+    state_spaces = filter_results(search_google(state, 25))
+    maker_list[state] = state_spaces
+
+    print(f"Saving {state} to JSON...")
+    with open("./py-scripts/spaceList.json", "r+") as json_file:
+        json.dump(maker_list, json_file)
+    sleep(4)
