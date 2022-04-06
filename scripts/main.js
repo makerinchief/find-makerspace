72017c7b0ee62feacd12f53ea5c09daeaf05b2d6
makerinchief <info@makerinchief.com>
2022-04-06 17:00:31 -0400

    Refactor
    Cleaned up everything

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,39 @@
-// Get the json file of us makerspaces from github
+// Get the json file of makerspaces from GitHub.
 let requestURL = 'https://raw.githubusercontent.com/intern-jck/findMakerspace/main/assets/json/spaceList.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-// Save contents of json
+// Save contents of JSON.
 let makerList = {};
 request.onload = function () {
   makerList = request.response;
 };
 
-// When the SVG loads, add events to each SVG path
+// When the SVG loads, add events to each SVG path.
 let mySvg = document.getElementById("us-map");
 mySvg.onload = function () {
-
-  // Get each state path
   let svgPaths = document.getElementById("us-map").children;
-  // Add the events to it
   for (let i = 0; i < svgPaths.length; i++) {
     addEvent(svgPaths[i]);
   }
 };
 
-// Add events to each state SVG
 function addEvent(state) {
 
-  // Get each state svg.
+  // Get state svg.
   element = document.getElementById(state.id);
-
-  // If mouse hover on state svg, show it's name
+  // Show name on mouse hover.
   element.addEventListener("mouseover", function () {
     document.getElementById("state-title").textContent = state.id.toUpperCase();
   });
-  // Reset title if not
+  // Reset title if not.
   element.addEventListener("mouseout", function () {
     document.getElementById("state-title").textContent = "PICK A STATE!";
   });
 
-  // If clicked, show that state's makerspace list
+  // If clicked, show that state's makerspace list.
   element.addEventListener("click", function () {
     updateMakerList(state.id);
     window.scrollBy(0, 400);
@@ -48,41 +43,36 @@ function addEvent(state) {
 
 function updateMakerList(stateId) {
 
-  // Get the block to display makerspace list
+  // Get the div to display makerspace list.
   let listContent = document.getElementById("list-content");
-  console.log(listContent)
 
-  // Create title for list
+  // Create title for list.
   document.getElementById("list-title").innerHTML = stateId.toUpperCase() + " Makerspaces";
 
-  // Clear any lists currently being shown
+  // Clear any lists currently being shown.
   while (listContent.firstChild) {
     listContent.removeChild(listContent.firstChild);
   }
 
-  // For each space in the list
+  // For each space in the list,
   for (let space in makerList[stateId]) {
 
-    // Make the space row
+    // make the name row,
     let spaceNameRow = document.createElement("div");
     spaceNameRow.classList.add("row");
-    spaceNameRow.classList.add("space-name-row");
-    spaceNameRow.classList.add("d-flex");
 
-    // Make the space name
+    // make the space name,
     var spaceName = document.createElement('h2');
     spaceName.innerHTML = makerList[stateId][space][0];
     spaceName.classList.add("pt-2");
     spaceName.classList.add("space-name");
 
-    // Make the link row
+    // make the link row,
     let spaceLinkRow = document.createElement("div");
     spaceLinkRow.classList.add("row");
-    spaceLinkRow.classList.add("space-link-row");
-    spaceLinkRow.classList.add("d-flex");
 
-    // Make the space link
-    var spaceLink = document.createElement('h3');
+    // make the space link,
+    var spaceLink = document.createElement('h2');
     var linkAnchor = document.createElement('a');
     linkAnchor.innerHTML = makerList[stateId][space][1];
     linkAnchor.setAttribute("href", makerList[stateId][space][1]);
@@ -91,11 +81,11 @@ function updateMakerList(stateId) {
     linkAnchor.classList.add("space-link");
     spaceLink.appendChild(linkAnchor);
 
-    // Add name and link to their rows
+    // add name and link to their rows,
     spaceNameRow.appendChild(spaceName);
     spaceLinkRow.appendChild(spaceLink);
 
-    // Add the rows to the page
+    // then add the rows to the page.
     listContent.appendChild(spaceNameRow);
     listContent.appendChild(spaceLinkRow);
   }
