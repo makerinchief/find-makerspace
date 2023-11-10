33c6bbeee0246ff73bca359dec2f90cf6747cdcc
Justin Kirk <justin.c.kirk@gmail.com>
2023-11-10 11:34:31 -0500

    backup

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 // Get the json file of makerspaces from GitHub.
-let requestURL = 'https://raw.githubusercontent.com/intern-jck/findMakerspace/main/assets/json/spaceList.json';
+// let requestURL = 'https://raw.githubusercontent.com/intern-jck/findMakerspace/main/assets/json/spaceList.json';
+let requestURL = '../assets/json/satte_list.json';
+
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
@@ -24,46 +26,44 @@ request.onload = function () {
 // };
 
 window.addEventListener('load', () => {
-  let mySvg = document.getElementById("us-map");
-  let svgPaths = document.getElementById("us-map").children;
-  console.log(svgPaths)
+  let mySvg = document.getElementById('us-map');
+  let svgPaths = document.getElementById('us-map').children;
+  console.log(svgPaths);
   for (let i = 0; i < svgPaths.length; i++) {
     addStateMouseEvents(svgPaths[i]);
   }
 });
 
 function addStateMouseEvents(state) {
-
   // Get state svg.
   element = document.getElementById(state.id);
   // Show name on mouse hover.
-  element.addEventListener("mouseover", function () {
-    document.getElementById("state-title").textContent = state.id.replace('-', ' ').toUpperCase();
+  element.addEventListener('mouseover', function () {
+    document.getElementById('state-title').textContent = state.id.replace('-', ' ').toUpperCase();
   });
 
   // Reset title if not.
-  element.addEventListener("mouseout", function () {
-    document.getElementById("state-title").textContent = "PICK A STATE!";
+  element.addEventListener('mouseout', function () {
+    document.getElementById('state-title').textContent = 'PICK A STATE!';
   });
 
   // If clicked, show that state's makerspace list.
-  element.addEventListener("click", function () {
+  element.addEventListener('click', function () {
     updateMakerList(state.id);
     const windowHeight = window.innerHeight;
     // A little hacky but should adjust so window scrolls into view on different screens
     window.scrollBy(0, windowHeight - 200);
   });
-
-};
+}
 
 function updateMakerList(stateId) {
-  console.log(stateId)
+  console.log(stateId);
 
   // Get the div to display makerspace list.
-  let listContent = document.getElementById("list-content");
+  let listContent = document.getElementById('list-content');
 
   // Create title for list.
-  document.getElementById("list-title").innerHTML = stateId.toUpperCase() + " Makerspaces";
+  document.getElementById('list-title').innerHTML = stateId.toUpperCase() + ' Makerspaces';
 
   // Clear any lists currently being shown.
   while (listContent.firstChild) {
@@ -72,29 +72,28 @@ function updateMakerList(stateId) {
 
   // For each space in the list,
   for (let space in makerList[stateId]) {
-
     // make the name row,
-    let spaceNameRow = document.createElement("div");
-    spaceNameRow.classList.add("row");
+    let spaceNameRow = document.createElement('div');
+    spaceNameRow.classList.add('row');
 
     // make the space name,
     var spaceName = document.createElement('h2');
     spaceName.innerHTML = makerList[stateId][space][0];
-    spaceName.classList.add("pt-2");
-    spaceName.classList.add("space-name");
+    spaceName.classList.add('pt-2');
+    spaceName.classList.add('space-name');
 
     // make the link row,
-    let spaceLinkRow = document.createElement("div");
-    spaceLinkRow.classList.add("row");
+    let spaceLinkRow = document.createElement('div');
+    spaceLinkRow.classList.add('row');
 
     // make the space link,
     var spaceLink = document.createElement('h2');
     var linkAnchor = document.createElement('a');
     linkAnchor.innerHTML = makerList[stateId][space][1];
-    linkAnchor.setAttribute("href", makerList[stateId][space][1]);
-    linkAnchor.setAttribute("target", "_blank");
-    linkAnchor.classList.add("pb-2");
-    linkAnchor.classList.add("space-link");
+    linkAnchor.setAttribute('href', makerList[stateId][space][1]);
+    linkAnchor.setAttribute('target', '_blank');
+    linkAnchor.classList.add('pb-2');
+    linkAnchor.classList.add('space-link');
     spaceLink.appendChild(linkAnchor);
 
     // add name and link to their rows,
